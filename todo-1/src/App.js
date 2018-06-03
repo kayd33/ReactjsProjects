8658c8ebbfe7068b34ec939c97fe38db5530d9f6
KaustubhD <kaustubhd287@gmail.com>
2018-06-03 12:57:22 +0530

    Day 1 (Added remove Functionality to 1)

--- todo-1/src/App.js
+++ todo-1/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class ToDoList extends Component{
+
+  clickHandler = (e) => {
+    this.props.removeFn(e.target.innerHTML);
+  }
   render(){
-    let listAsLi = this.props.list.map((element) => {
-      return <li>{element}</li>
+    let listAsLi = this.props.list.map((element, index) => {
+      return <li key={index} onClick={this.clickHandler}>{element}</li>
     });
     return(
       <ul>{listAsLi}</ul>
@@ -43,16 +47,20 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {list: ['One', 'Two']};
+    this.state = {list: []};
   }
   addHandler = (item) => {
     this.setState({list: this.state.list.concat(item)});
+  }
+  removeHandler = (item) => {
+    this.setState({list: this.state.list.filter(element => item !== element)});
   }
 
   render() {
     return (
       <div style={{backgroundColor: '#666', padding: '20px 0'}}>
-        <ToDoList list={this.state.list} />
+        <h3>Add your tasks using the text box and remove them by clicking on your tasks</h3>
+        <ToDoList removeFn={this.removeHandler} list={this.state.list} />
         <NewTodos addFn={this.addHandler}/>
       </div>
     );
